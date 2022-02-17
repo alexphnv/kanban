4b2a1f2d9e05e2a6e7390ce2780812989d22a690
alexnv <alexphnv@gmail.com>
2022-02-17 14:08:10 +0300

    test commits

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let draggables = document.querySelectorAll('.draggable')
 const taskcontainers = document.querySelectorAll('.taskcontainer')
 let taskId = 0;
 
-(function () { //get tasks from localstorage
+(function () { //get tasks from localstorages
     let tasksFromLS = []
     Object.keys(localStorage).forEach(task => tasksFromLS.
         push(JSON.parse(localStorage.getItem(task))))
